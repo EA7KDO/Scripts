4509c931ff327f7fd29ac1089e9d2c5c796900d4
root <root@pi-star>
2020-04-17 17:35:48 -0400

    Updated to handle 3.2 inch screen and case insensitive

--- gitcopy.sh
+++ gitcopy.sh
@@ -19,96 +19,140 @@ if [ -z "$1" ]; then
 	exit
 fi
 
+s1="NX3224K024"
+s2="NX4024K032"
+s3="NX4832K035"
+errtext="Error! - Aborting"
 
-scn="$1"
+## Strip off .tft - Take only the first 10 characters
+scn=$(echo "$1" | tr [:lower:] [:upper:])
+#tr [:lower:] [:upper:]
 scn="${scn:0:10}"
 
+##Set Call to either 1 for EA7KDO or 2 for VE3RD 
 call="$2"
+#Feed back is off by default - Turn it on with anything as parameter 3
 fb="$3"
+#Set Default mode to EA7KDO
 calltxt="EA7KDO"
 
 #echo "$scn"
 
-
+## Programmed Shutdown
 function exitcode
 {
+	echo "Script Execution Failed "
 	echo "$scn"
-	echo "Program Execution Halted"
+	echo "$errtext"
 	exit
 
 }
 
+# EA7KDO Script Function
 function getea7kdo
 {
+	tst=0
 #	echo "Function EA7KDO"
 	calltxt="EA7KDO"
 
-    	if [ "$scn" = "NX3224K024" ]; then
+    	if [ "$scn" == "NX3224K024" ]; then
+
+		##  New Github Location  for the 32 inch screen - Remove the comment # when active
+	#	sudo git clone --depth 1 https://github.com/EA7KDO/NX3224K024 /home/pi-star/Nextion_Temp
+
+		### Old Github Location for the 24 ionch screen - Remove this when the new location becomes active
 	  	sudo git clone --depth 1 https://github.com/EA7KDO/Nextion.Images /home/pi-star/Nextion_Temp
 		tst=1
 	fi     
-	if [ "$scn" = "NX4832K035" ]; then
+	if [ "$scn" == "NX4832K035" ]; then
 	  	sudo git clone --depth 1 https://github.com/EA7KDO/NX4832K035 /home/pi-star/Nextion_Temp
 		tst=2
      	fi
 	
-	if [ "$tst" = 0 ]; then
-		exitcode "Invalid EA7KDO Screen Name $scn"
+	if [ "$tst" == 0 ]; then
+		errtext="Invalid EA7KDO Screen Name $scn"	
+		exitcode 
 	fi
 }
 
+# VE3RD Script Function
 function getve3rd
 {
+	tst=0
 #	echo "Function VE3RD"
      	
 	calltxt="VE3RD"
-	if [ "$scn" = "NX3224K024" ]; then	
-  	  sudo git clone --depth 1 https://github.com/VE3RD/Nextion /home/pi-star/Nextion_Temp
-	else
-		exitcode "Invalid VE3RD Screen Name $scn"
+	if [ "$scn" = "$s1" ]; then	
+	 	tst=1  
+	  	sudo git clone --depth 1 https://github.com/VE3RD/Nextion /home/pi-star/Nextion_Temp
+	fi
+	if [ "$scn" = "$s2" ]; then
+		tst=2	
+  	  	sudo git clone --depth 1 https://github.com/VE3RD/Nextion /home/pi-star/Nextion_Temp
+	fi
+	if [ "$tst" = 0 ]; then
+		errtext="Invalid VE3RD Screen Name $scn,  $s1,  $s2"
+		exitcode 
 	fi
 
 }
 
+#### Start of Main Code
 
-if [ -z "$1" ]; then
-	echo " Syntax: gitcopy.sh NX????K???   // Will copy EA7KDO Files - Default"
+# Usage Syntax if no parameters are supplies
+if [ -z "$scn" ]; then
+	echo " Syntax: gitcopy.sh NX????K???   // Will copy EA7KDO Files - Default for Screen"
 	echo " Syntax: gitcopy.sh NX????K??? 1 // Will copy EA7KDO Files - Selected"
 	echo " Syntax: gitcopy.sh NX????K??? 2 // Will copy VE3RD Files - Selected"
 	echo " Adding a third parameter(anything) will provide feedback as the script runs (Commandline)"
 	echo " " 
+        echo  " Valid Screens - EA7KDO) - NX3224K024, NX4832K035"
+        echo  " Valid Screens - VE3RD ) - NX3224K024, NX4032K032"
+	echo " "
 fi
 
+#Process Extra Parameters
 if [ -z "$2" ]; then
+#  echo "Setting Default=EA7KDO"
   call="1"
 fi
-
-if [ "$call" = "0" ]; then
+if [ "$scn" == "$s2" ]; then
+#  echo "Override - Setting VE3RD for $s2"
   call="2"
+fi
+
+if [ "$call" == "0" ]; then
+#  echo "Call Parameter set to 0 - Ea7KDO"
+  call="1"
   fb="2"
 fi
 
-
+#echo "$scn  - $call" 
 if [ "$fb" ]; then
 	if [ "$call" == "1" ]; then
-	 	echo "Loading EA7KDO $scn Screen Package"
-	fi
+	        if [ "$scn" != "$s1" -a "$scn" != "$s3" ]; then
+              		echo "EA7KDO Screen Name MUST be NX3224K024 or NX4832K035"
+                       	errtext="Invalid EA7KDO Screen Name"
+                        exitcode
+		else
+			echo "Loading EA7KDO $scn Screen Package"
+
+                fi
+
+        fi
+
 	if [ "$call" == "2" ]; then
-                if [ ! "$scn" = "NX3224K024" ]; then
+                if [ "$scn" != "$s1" -a "$scn" != "$s2" ]; then
+			echo "VE3RD Screen Name MUST be NX3224K024 or NX4024K024"
+			errtext="Invalid VE3RD Screen Name"
+			exitcode
+		else
 			echo "Loading VE3RD $scn Screen Package"
 		fi	
 	fi
 fi
 
-if [ "$call" = "2" ]; then
-     	if [ "$scn" != "NX3224K024" ]; then
-		if [ "$fb" ]; then
-			scn="NX3224K024"
-			echo "VE3RD Screen Name MUST be NX3224K024"
-			echo "Revising Screen Name to Match VE3RD Screens"
-		fi
-	fi
-fi
+#echo " End Processing Parameters  - $scn $call"
 
 if [ ! -d /home/pi-star/Nextion_Temp ]; then
    sudo mkdir /home/pi-star/Nextion_Temp
@@ -125,7 +169,8 @@ if [ ! "$fb" ]; then
 fi
 
 model="$scn"
-tft='.tft' gz='.gz'
+tft='.tft' 
+#gz='.gz'
 #Put Pi-Star file system in RW mode
 sudo mount -o remount,rw /
 sleep 1s
@@ -146,14 +191,14 @@ fi
   # Get EA7KDO File Set
 
 tst=0
-if [ "$call" = "1" ]; then
+if [ "$call" == "1" ]; then
 	getea7kdo
  
 fi
 
 
   # Get VE3RD File Set
-if [ "$call" = "2" ]; then
+if [ "$call" == "2" ]; then
 	getve3rd
 fi
 
@@ -175,12 +220,13 @@ if [ -f /home/pi-star/Nextion_Temp/profiles.txt ]; then
 	fi
 fi
 
+model="$scn"
 if [ "$fb" ]; then
     echo "Remove Existing $model$tft and copy in the new one"
 fi
 
 if [ -f /usr/local/etc/$model$tft ]; then
-	sudo rm /usr/local/etc/$model$tft
+	sudo rm /usr/local/etc/NX*K*.tft
 fi
 sudo cp /home/pi-star/Nextion_Temp/$model$tft /usr/local/etc/
 
@@ -189,6 +235,7 @@ sudo cp /home/pi-star/Nextion_Temp/$model$tft /usr/local/etc/
  if [ ! -f "$FILE" ]; then
         # Copy failed
       echo "No TFT File Available to Flash - Try Again"
+	errtext="Missing tft File Parameter"
 	exitcode
  fi
 
