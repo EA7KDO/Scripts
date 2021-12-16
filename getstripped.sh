aa3d3288767fc3a0b1c3bea1591368975b8e0068
root <root@pi-star>
2021-12-15 21:09:45 -0500

    updated

--- getstripped.sh
+++ getstripped.sh
@@ -8,5 +8,7 @@ set -o pipefail
 sudo mount -o remount,rw /
 
 wget https://database.radioid.net/static/user.csv -O /usr/local/etc/stripped.csv
-
+sudo sed -i 's/United States/US/g' /usr/local/etc/stripped.csv
 sudo mount -o remount,ro /
+
+
