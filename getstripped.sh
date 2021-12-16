31cd463f225d1b23c2a40f9f3babcaba136132a6
root <root@pi-star>
2021-12-15 20:24:52 -0500

    added

---
+++ getstripped.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+############################################################
+#  Get User ID Database                                    #
+#  VE3RD                                      2020-05-20   #
+############################################################
+set -o errexit
+set -o pipefail
+sudo mount -o remount,rw /
+
+wget https://database.radioid.net/static/user.csv -O /usr/local/etc/stripped.csv
+
+sudo mount -o remount,ro /
