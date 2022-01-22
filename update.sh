af1f15ecc78d3009700feaa3fbe2423fa87d526c
phil <ve3rd@rac.ca>
2022-01-22 13:39:07 -0500

    update

--- update.sh
+++ update.sh
@@ -10,5 +10,8 @@ set -o pipefail
 cd /home/pi-star/Scripts
 sudo mount -o remount,rw /
 sudo git pull
+wget https://database.radioid.net/static/user.csv -O /usr/local/etc/user.csv
+sudo cp /usr/local/etc/user.csv /usr/local/etc/stripped.csv
+#sudo sed -i 's/United States/USA/g' /usr/local/etc/stripped.csv
 sudo mount -o remount,ro /
 
