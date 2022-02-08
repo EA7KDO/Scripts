9c685a641dd9884235cf9f8364e73251e47f57c3
phil <ve3rd@rac.ca>
2022-02-08 14:25:34 -0500

    updates

--- getstripped.sh
+++ getstripped.sh
@@ -11,7 +11,7 @@ set -o pipefail
 sudo mount -o remount,rw /
 
 sudo wget https://database.radioid.net/static/user.csv -O /usr/local/etc/stripped.csv
-cp /usr/local/etc/stripped.csv /usr/local/etc/users.csv
 sudo sed -i 's/United States/USA/g' /usr/local/etc/stripped.csv
+cp /usr/local/etc/stripped.csv /usr/local/etc/users.csv
 sudo mount -o remount,ro /
 
